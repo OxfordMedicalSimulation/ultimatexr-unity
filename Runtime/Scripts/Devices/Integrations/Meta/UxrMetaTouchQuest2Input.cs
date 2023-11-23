49a8575c7b247b225e34c3c0f94390eba9c4205b
leehall-oms <lee.hall@oxfordmedicalsimulation.com>
2023-11-23 12:15:25 +0000

    Fixed typo

--- Runtime/Scripts/Devices/Integrations/Meta/UxrMetaTouchQuest2Input.cs
+++ Runtime/Scripts/Devices/Integrations/Meta/UxrMetaTouchQuest2Input.cs
@@ -60,7 +60,7 @@ namespace UltimateXR.Devices.Integrations.Meta
             get
             {
                 if (UxrTrackingDevice.HeadsetDeviceName is "Oculus Quest2" || 
-                    || UxrTrackingDevice.HeadsetDeviceName is "Meta Quest 3")
+                    UxrTrackingDevice.HeadsetDeviceName is "Meta Quest 3")
                 {
                     yield return "Oculus Touch Controller - Left";
                     yield return "Oculus Touch Controller - Right";
